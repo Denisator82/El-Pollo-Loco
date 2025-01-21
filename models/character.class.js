0f78fff8e10f13ec7e8c2355c77ccfe950fc4574
Denisator82 <denis.welsch@gmx.de>
2025-01-21 17:32:47 +0100

    request animation Frame

--- models/character.class.js
+++ models/character.class.js
@@ -1,5 +1,10 @@
 class Character extends MovableObject{
 
+
+    constructor(){
+        super().loadImage('img/img_pollo_locco/img/2_character_pepe/2_walk/W-21.png');
+    }
+
     jump(){
 
     }
