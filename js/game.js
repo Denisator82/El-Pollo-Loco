37fcc8e359132a02c85af7913af63cd76b994f59
Denisator82 <denis.welsch@gmx.de>
2025-01-21 13:29:54 +0100

    create classes

--- js/game.js
+++ js/game.js
@@ -1,14 +1,14 @@
 let canvas;
 let ctx;
-let character = new Image();
+
+let world = new World();
+
 
 function init(){
     canvas = document.getElementById('canvas');
     ctx = canvas.getContext('2d');
 
 
+    console.log('My Character is', world.character);
 
-    character.src = '../img/img_pollo_locco/img/2_character_pepe/2_walk/W-21.png';
-
-    ctx.drawImage(character, 20, 300, 50, 150);
 }
