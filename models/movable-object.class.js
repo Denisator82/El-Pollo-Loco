e3b473bec3635dba027faf6a486126ccfab47464
Denisator82 <denis.welsch@gmx.de>
2025-01-21 18:01:39 +0100

    create position clouds

--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -1,12 +1,13 @@
 class MovableObject {
     x = 120;
     y = 250;
-    img;
     height = 150;
     width = 100;
+    img;
 
+    //loadImage('img/test.png')
     loadImage(path) {
-        this.img = new Image();
+        this.img = new Image(); //this.img = document.getElementById('image') <img id="image" src>
         this.img.src= path; 
     }
 
