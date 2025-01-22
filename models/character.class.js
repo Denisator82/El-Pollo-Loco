4b131f92bdf0f9132dcdf0e09a039f77a8c8543b
Denisator82 <denis.welsch@gmx.de>
2025-01-22 17:08:56 +0100

    character move

--- models/character.class.js
+++ models/character.class.js
@@ -2,6 +2,7 @@ class Character extends MovableObject{
     y = 150;
     height = 280;
     width = 120;
+    speed = 10;
     IMAGES_WALKING = [
         'img/img_pollo_locco/img/2_character_pepe/2_walk/W-21.png',
         'img/img_pollo_locco/img/2_character_pepe/2_walk/W-22.png',
@@ -20,15 +21,28 @@ class Character extends MovableObject{
     }
 
     animate(){
+
+        setInterval(() => {
+            if(this.world.keyboard.RIGHT) {
+                this.x += this.speed;
+            }
+
+            if(this.world.keyboard.LEFT) {
+                this.x -= this.speed;
+            }
+        }, 1000 / 60);
+
+
         setInterval(() => {
 
-            if(this.world.keyboard.RIGHT) {
+            if(this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
+                // Walk animation
                 let i = this.currentImage % this.IMAGES_WALKING.length; // let i = 7 % 6; => 1, Rest 1
                 let path = this.IMAGES_WALKING[i];
                 this.img = this.imageCache[path];
                 this.currentImage++;
             }    
-        }, 100);
+        }, 50);
     }
 
     jump(){
