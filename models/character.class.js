eee7b6677eafac681bbc1521ab7bbbae35f9adef
Denisator82 <denis.welsch@gmx.de>
2025-01-22 07:53:12 +0100

    optimization of the size of characters and enemies

--- models/character.class.js
+++ models/character.class.js
@@ -1,5 +1,7 @@
 class Character extends MovableObject{
-
+    y = 150;
+    height = 280;
+    width = 120;
 
     constructor(){
         super().loadImage('img/img_pollo_locco/img/2_character_pepe/2_walk/W-21.png');
