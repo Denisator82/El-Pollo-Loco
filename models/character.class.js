e2389bb5b60481814a8367bebf30c34a9ec71ba5
Denisator82 <denis.welsch@gmx.de>
2025-01-22 10:20:00 +0100

    character animate

--- models/character.class.js
+++ models/character.class.js
@@ -2,9 +2,30 @@ class Character extends MovableObject{
     y = 150;
     height = 280;
     width = 120;
+    IMAGES_WALKING = [
+        'img/img_pollo_locco/img/2_character_pepe/2_walk/W-21.png',
+        'img/img_pollo_locco/img/2_character_pepe/2_walk/W-22.png',
+        'img/img_pollo_locco/img/2_character_pepe/2_walk/W-23.png',
+        'img/img_pollo_locco/img/2_character_pepe/2_walk/W-24.png',
+        'img/img_pollo_locco/img/2_character_pepe/2_walk/W-25.png',
+        'img/img_pollo_locco/img/2_character_pepe/2_walk/W-26.png'
+    ];
+    currentImage = 0;
 
     constructor(){
         super().loadImage('img/img_pollo_locco/img/2_character_pepe/2_walk/W-21.png');
+        this.loadImages(this.IMAGES_WALKING);
+
+        this.animate();
+    }
+
+    animate(){
+        setInterval(() => {
+        let i = this.currentImage % this.IMAGES_WALKING.length; // let i = 0 % 6; => 1, Rest 0
+        let path = this.IMAGES_WALKING[i];
+        this.img = this.imageCache[path];
+        this.currentImage++;
+        }, 100);
     }
 
     jump(){
