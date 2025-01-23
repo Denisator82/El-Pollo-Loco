e2179426754cc1457e1e15af065ea19e60ccd902
Denisator82 <denis.welsch@gmx.de>
2025-01-23 13:29:54 +0100

    character walking sound

--- models/character.class.js
+++ models/character.class.js
@@ -12,6 +12,7 @@ class Character extends MovableObject{
         'img/img_pollo_locco/img/2_character_pepe/2_walk/W-26.png'
     ];
     world;
+    walking_sound = new Audio('audio/running_sand.mp3');
     
     constructor(){
         super().loadImage('img/img_pollo_locco/img/2_character_pepe/2_walk/W-21.png');
@@ -23,14 +24,17 @@ class Character extends MovableObject{
     animate(){
 
         setInterval(() => {
+            this.walking_sound.pause();
             if(this.world.keyboard.RIGHT && this.x < this.world.level.level_end_x) {
                 this.x += this.speed;
                 this.otherDirection = false;
+                this.walking_sound.play();
             }
 
             if(this.world.keyboard.LEFT && this.x > 0) {
                 this.x -= this.speed;
                 this.otherDirection = true;
+                this.walking_sound.play();
             }
             this.world.camera_x = -this.x + 100;
         }, 1000 / 60);
