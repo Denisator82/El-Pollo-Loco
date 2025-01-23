107d88c4fc5f4facdb4958eeb137bc8eba797026
Denisator82 <denis.welsch@gmx.de>
2025-01-23 08:33:54 +0100

    camera move & Insert background objects

--- models/world.class.js
+++ models/world.class.js
@@ -9,14 +9,34 @@ class World {
         new Cloud()
     ];
     backgroundObjects = [
-        new BackgroundObject('img/img_pollo_locco/img/5_background/layers/air.png',0 , 80),
-        new BackgroundObject('img/img_pollo_locco/img/5_background/layers/3_third_layer/1.png', 0, 80),
-        new BackgroundObject('img/img_pollo_locco/img/5_background/layers/2_second_layer/1.png', 0, 80),
-        new BackgroundObject('img/img_pollo_locco/img/5_background/layers/1_first_layer/1.png', 0, 80)
+        new BackgroundObject('img/img_pollo_locco/img/5_background/layers/air.png', -719),
+        new BackgroundObject('img/img_pollo_locco/img/5_background/layers/3_third_layer/2.png', -719),
+        new BackgroundObject('img/img_pollo_locco/img/5_background/layers/2_second_layer/2.png', -719),
+        new BackgroundObject('img/img_pollo_locco/img/5_background/layers/1_first_layer/2.png', -719),
+
+        new BackgroundObject('img/img_pollo_locco/img/5_background/layers/air.png',0),
+        new BackgroundObject('img/img_pollo_locco/img/5_background/layers/3_third_layer/1.png', 0),
+        new BackgroundObject('img/img_pollo_locco/img/5_background/layers/2_second_layer/1.png', 0),
+        new BackgroundObject('img/img_pollo_locco/img/5_background/layers/1_first_layer/1.png', 0),
+        new BackgroundObject('img/img_pollo_locco/img/5_background/layers/air.png',719),
+        new BackgroundObject('img/img_pollo_locco/img/5_background/layers/3_third_layer/2.png', 719),
+        new BackgroundObject('img/img_pollo_locco/img/5_background/layers/2_second_layer/2.png', 719),
+        new BackgroundObject('img/img_pollo_locco/img/5_background/layers/1_first_layer/2.png', 719),
+
+        new BackgroundObject('img/img_pollo_locco/img/5_background/layers/air.png', 719*2),
+        new BackgroundObject('img/img_pollo_locco/img/5_background/layers/3_third_layer/1.png', 719*2),
+        new BackgroundObject('img/img_pollo_locco/img/5_background/layers/2_second_layer/1.png', 719*2),
+        new BackgroundObject('img/img_pollo_locco/img/5_background/layers/1_first_layer/1.png', 719*2),
+        new BackgroundObject('img/img_pollo_locco/img/5_background/layers/air.png', 719*3),
+        new BackgroundObject('img/img_pollo_locco/img/5_background/layers/3_third_layer/2.png', 719*3),
+        new BackgroundObject('img/img_pollo_locco/img/5_background/layers/2_second_layer/2.png', 719*3),
+        new BackgroundObject('img/img_pollo_locco/img/5_background/layers/1_first_layer/2.png', 719*3),
+  
     ];
     ctx;
     canvas;
     keyboard;
+    camera_x = 0;
 
     constructor(canvas, keyboard){
         this.ctx = canvas.getContext('2d');
@@ -33,11 +53,17 @@ class World {
     draw() {
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
+        this.ctx.translate(this.camera_x, 0);
+
         this.addObjectsToMap(this.backgroundObjects);
+
         this.addToMap(this.character);
-        this.addObjectsToMap(this.enemies);
         this.addObjectsToMap(this.clouds);
+        this.addObjectsToMap(this.enemies);
         
+        this.ctx.translate(-this.camera_x, 0);
+
+
         //Draw() wird immer wieder eingefügt 
         let self = this;
         requestAnimationFrame(function() {
