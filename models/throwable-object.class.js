432afd10bf7078975efbdee837237078e00ca724
Denisator82 <denis.welsch@gmx.de>
2025-01-27 16:49:22 +0100

    integrated Images for bottle rotation & splash

--- models/throwable-object.class.js
+++ models/throwable-object.class.js
@@ -1,7 +1,25 @@
 class ThrowableObject extends MovableObject {
 
+    IMAGES_ROTATION = [
+        'img/img_pollo_locco/img/6_salsa_bottle/bottle_rotation/1_bottle_rotation.png',
+        'img/img_pollo_locco/img/6_salsa_bottle/bottle_rotation/2_bottle_rotation.png',
+        'img/img_pollo_locco/img/6_salsa_bottle/bottle_rotation/3_bottle_rotation.png',
+        'img/img_pollo_locco/img/6_salsa_bottle/bottle_rotation/4_bottle_rotation.png',
+    ];
+
+    IMAGES_SPLASH = [
+        'img/img_pollo_locco/img/6_salsa_bottle/bottle_rotation/bottle_splash/1_bottle_splash.png',
+        'img/img_pollo_locco/img/6_salsa_bottle/bottle_rotation/bottle_splash/2_bottle_splash.png',
+        'img/img_pollo_locco/img/6_salsa_bottle/bottle_rotation/bottle_splash/3_bottle_splash.png',
+        'img/img_pollo_locco/img/6_salsa_bottle/bottle_rotation/bottle_splash/4_bottle_splash.png',
+        'img/img_pollo_locco/img/6_salsa_bottle/bottle_rotation/bottle_splash/5_bottle_splash.png',
+        'img/img_pollo_locco/img/6_salsa_bottle/bottle_rotation/bottle_splash/6_bottle_splash.png',
+    ];
+
     constructor(x, y) {
         super().loadImage('img/img_pollo_locco/img/6_salsa_bottle/salsa_bottle.png');
+        this.loadImages(this.IMAGES_ROTATION);
+        this.loadImages(this.IMAGES_SPLASH);
         this.x = x;
         this.y = y;
         this.height = 60;
