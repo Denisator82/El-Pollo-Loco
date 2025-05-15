b03da0324db6bc60087b23227c079bb50dc8f48d
Denisator82 <denis.welsch@gmx.de>
2025-05-15 09:34:36 +0200

    docs: add JSDoc to chicken-mini.class.js

--- models/chicken-mini.class.js
+++ models/chicken-mini.class.js
@@ -1,22 +1,76 @@
+/**
+ * Represents a small enemy chicken in the game.
+ * Inherits from {@link MovableObject}.
+ */
 class ChickenMini extends MovableObject {
+  /**
+   * Vertical position of the chicken.
+   * @type {number}
+   */
   y = 360;
+
+  /**
+   * Height of the chicken.
+   * @type {number}
+   */
   height = 60;
+
+  /**
+   * Width of the chicken.
+   * @type {number}
+   */
   width = 60;
+
+  /**
+   * Damage dealt by the chicken to the character.
+   * @type {number}
+   */
   damage = 20;
+
+  /**
+   * Whether the chicken is dead.
+   * @type {boolean}
+   */
   chickenIsDead = false;
+
+  /**
+   * Tracks if the death sound was already played.
+   * @type {boolean}
+   */
   deathSoundPlayed = false;
 
+  /**
+   * Walking animation images.
+   * @type {string[]}
+   */
   IMAGES_WALKING = [
     "img/img/3_enemies_chicken/chicken_small/1_walk/1_w.png",
     "img/img/3_enemies_chicken/chicken_small/1_walk/2_w.png",
     "img/img/3_enemies_chicken/chicken_small/1_walk/3_w.png",
   ];
 
+  /**
+   * Death animation image.
+   * @type {string[]}
+   */
   IMAGES_DEAD = ["img/img/3_enemies_chicken/chicken_small/2_dead/dead.png"];
 
+  /**
+   * ID of the movement interval.
+   * @type {number|null}
+   */
   moveIntervalId = null;
+
+  /**
+   * ID of the animation interval.
+   * @type {number|null}
+   */
   animationIntervalId = null;
 
+  /**
+   * Creates a new small chicken enemy.
+   * Initializes animations, position, speed, and collision offset.
+   */
   constructor() {
     super().loadImage(this.IMAGES_WALKING[0]);
     this.loadImages(this.IMAGES_WALKING);
@@ -28,20 +82,33 @@ class ChickenMini extends MovableObject {
     this.animate();
   }
 
+  /**
+   * Returns whether the chicken is dead.
+   * @returns {boolean}
+   */
   isDead() {
     return this.chickenIsDead;
   }
 
+  /**
+   * Marks the chicken as dead.
+   */
   setDead() {
     this.chickenIsDead = true;
   }
 
+  /**
+   * Moves the chicken to the left if not dead.
+   */
   moveChicken() {
     if (!this.chickenIsDead) {
       this.x -= this.speed;
     }
   }
 
+  /**
+   * Plays the appropriate animation depending on chicken state.
+   */
   chickenAnimation() {
     if (!this.chickenIsDead) {
       this.chickenAnimationWalk();
@@ -50,14 +117,25 @@ class ChickenMini extends MovableObject {
     }
   }
 
+  /**
+   * Plays the walking animation.
+   */
   chickenAnimationWalk() {
     this.playAnimation(this.IMAGES_WALKING);
   }
 
+  /**
+   * Plays the death animation and sound.
+   * Removes death image after a delay.
+   */
   chickenAnimationDead() {
     this.playAnimation(this.IMAGES_DEAD);
 
-    if (!this.deathSoundPlayed && this.world?.audioManager && !this.world.audioManager.isMuted) {
+    if (
+      !this.deathSoundPlayed &&
+      this.world?.audioManager &&
+      !this.world.audioManager.isMuted
+    ) {
       this.world.audioManager.playSound("chickenDead");
       this.deathSoundPlayed = true;
     }
@@ -67,6 +145,9 @@ class ChickenMini extends MovableObject {
     }, 500);
   }
 
+  /**
+   * Starts movement and animation intervals.
+   */
   animate() {
     this.moveIntervalId = setInterval(() => {
       this.moveChicken();
@@ -77,6 +158,9 @@ class ChickenMini extends MovableObject {
     }, 200);
   }
 
+  /**
+   * Stops movement and animation intervals.
+   */
   stopChickenMiniIntervals() {
     clearInterval(this.moveIntervalId);
     clearInterval(this.animationIntervalId);
