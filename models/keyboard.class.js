9f86a23295fd917a701af660895f42318a9b9fd6
Denisator82 <denis.welsch@gmx.de>
2025-05-15 09:48:22 +0200

    docs: add JSDoc to keyboard.class.js

--- models/keyboard.class.js
+++ models/keyboard.class.js
@@ -1,12 +1,41 @@
 /**
  * Represents the state of keyboard keys used in the game.
- * Each property corresponds to a key and indicates whether it is pressed (true) or not (false).
+ * Each property corresponds to a specific key and is set to `true` when pressed, `false` when released.
  */
 class Keyboard {
+    /**
+     * Left arrow key or A.
+     * @type {boolean}
+     */
     LEFT = false;
+
+    /**
+     * Right arrow key or D.
+     * @type {boolean}
+     */
     RIGHT = false;
+
+    /**
+     * Up arrow key or W.
+     * @type {boolean}
+     */
     UP = false;
+
+    /**
+     * Down arrow key or S.
+     * @type {boolean}
+     */
     DOWN = false;
+
+    /**
+     * Space key – used for jumping.
+     * @type {boolean}
+     */
     SPACE = false;
+
+    /**
+     * Shift key – used for throwing bottles.
+     * @type {boolean}
+     */
     SHIFT = false;
 }
